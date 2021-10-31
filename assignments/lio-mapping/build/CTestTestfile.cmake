3c1113436478a68cb00b6ecdd46a55f3cf31f838
Zhiwei  Chen <im.zwei.chen@gmail.com>
2021-10-31 13:46:16 +0800

    add_L10_hw

--- assignments/lio-mapping/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/assignments/lio-mapping/src
-# Build directory: /workspace/assignments/lio-mapping/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lio-mapping-master")
